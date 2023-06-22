fb7703f19875739e78c2df0c1ad595670cf776a5
Tomas Kohout <tomas.kohout@showmax.com>
2023-06-22 11:45:11 +0200

    Initial Commit

---
+++ VisionPlaybackApp/VisionPlaybackAppApp.swift
@@ -0,0 +1,21 @@
+//
+//  VisionPlaybackAppApp.swift
+//  VisionPlaybackApp
+//
+//  Created by Tomas Kohout on 22.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct VisionPlaybackAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+
+        ImmersiveSpace(id: "ImmersiveSpace") {
+            ImmersiveView()
+        }.immersionStyle(selection: .constant(.full), in: .full)
+    }
+}
